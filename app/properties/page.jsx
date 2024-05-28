173c9c16bdcb4f7a8f0d9f2c0525033098018bb9
Rafał H <rafal.hes@yahoo.com>
2024-05-28 13:01:27 +0200

    feat: add properties page (wip)

---
+++ app/properties/page.jsx
@@ -0,0 +1,17 @@
+import properties from '@/properties.json';
+
+const PropertiesPage = () => {
+  return (
+    <section class='px-4 py-6'>
+      <div class='container-xl lg:container m-auto'>
+        <div class='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          {properties.map((property) => (
+            <div>{property.name}</div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PropertiesPage;
