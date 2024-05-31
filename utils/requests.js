ca0192b98bf52f56fab4de8c512ed91164d180ab
Rafał H <rafal.hes@yahoo.com>
2024-05-31 12:11:31 +0200

    refactor: handle case where domain with properties is not available yet

--- utils/requests.js
+++ utils/requests.js
@@ -2,17 +2,18 @@ const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 async function fetchProperties() {
   try {
-    const res = await fetch(
-        // Handle the case where the damain is not availiable yet
-      `${apiDomain}/properties`,
-      { cache: 'no-store' }
-    );
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+    const res = await fetch(`${apiDomain}/properties`, { cache: 'no-store' });
     if (!res.ok) {
       throw new Error('Failed to fetch data');
     }
     return res.json();
   } catch (error) {
     console.log(error);
+    return [];
   }
 }
 
