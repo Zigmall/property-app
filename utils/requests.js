f98041069465af78881b5c71f06f07e3baa297d7
Rafał H <rafal.hes@yahoo.com>
2024-05-31 14:07:32 +0200

    feat: add function to fetch single property page

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+
+//Fetch all properties
 async function fetchProperties() {
   try {
     // Handle the case where the domain is not available yet
@@ -17,4 +19,22 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+//Fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`, { cache: 'no-store' });
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
