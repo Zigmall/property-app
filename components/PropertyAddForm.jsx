dc6524ef9128f09c3180f043fafa84ff60a76cc1
Rafał H <rafal.hes@yahoo.com>
2024-06-07 11:08:20 +0200

    feat: create object to get all data from PropertyAddForm fields (wip)

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -10,7 +10,7 @@ const PropertyAddForm = () => {
     location: {
       street: '',
       city: 'Test city',
-      state: '',
+      state: 'Test state',
       zipcode: '',
     },
     beds: '3',
@@ -102,7 +102,7 @@ const PropertyAddForm = () => {
 
   return (
     mounted && (
-      <form>
+      <form action='/api/properties' method='POST' encType='multipart/form-data' >
         <h2 className='text-3xl text-center font-semibold mb-6'>
           Add Property
         </h2>
@@ -564,6 +564,7 @@ const PropertyAddForm = () => {
             accept='image/*'
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
