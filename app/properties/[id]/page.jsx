eda964faae085db44d46b39146c692fd1290044a
Rafał H <rafal.hes@yahoo.com>
2024-06-08 12:51:07 +0200

    feat: add PropertyImages component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import Link from 'next/link';
 import { FaArrowLeft } from 'react-icons/fa';
 import PropertyDetails from '@/components/PropertyDetails';
+import PropertyImages from '@/components/PropertyImages';
 import Spinner from '@/components/Spinner';
 
 const PropertyPage = () => {
@@ -146,6 +147,7 @@ const PropertyPage = () => {
               </div>
             </div>
           </section>
+          <PropertyImages images={property.images} />
         </>
       )}
     </>
