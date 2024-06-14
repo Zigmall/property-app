ca367fb664bb5389268e7cfaf99b2bc0e1f0374c
Rafał H <rafal.hes@yahoo.com>
2024-06-14 22:54:08 +0200

    feat: add PropertyMap to PropertyDetails

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -6,6 +6,7 @@ import {
   FaCheck,
   FaMapMarker,
 } from 'react-icons/fa';
+import PropertyMap from '@/components/PropertyMap';
 const PropertyDetails = ({ property }) => {
   const { location, type, name, rates, description, beds, baths, square_feet } =
     property;
@@ -97,7 +98,7 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
-        <div id='map'></div>
+        <PropertyMap />
       </div>
     </main>
   );
