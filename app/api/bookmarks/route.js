71917805f813e0df6aa2bcba773c9bb7f277fcb0
Rafał H <rafal.hes@yahoo.com>
2024-06-17 14:12:13 +0200

    feat: add GET api route for bookmarks

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,8 +1,31 @@
 import connectDB from '@/config/database';
+import Property from '@/models/Property';
 import User from '@/models/User';
 import { getSessionUser } from '@/utils/getSessionUser';
 
 export const dynamic = 'force-dynamic';
+
+// GET /api/bookmarks
+export const GET = async () => {
+  try {
+    connectDB();
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.user) {
+      return new Response('User ID is required', { status: 401 });
+    }
+    const { userId } = sessionUser;
+
+    // Find User in database
+    const user = await User.findOne({ _id: userId });
+
+    // Get User's bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+    return new Response(JSON.stringify(bookmarks, { status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
 
 export const POST = async (request) => {
   try {
